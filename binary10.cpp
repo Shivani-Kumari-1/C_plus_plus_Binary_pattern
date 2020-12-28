cc447d8969663ff1127c3baa13fdf4446c4a062d
Shivani Kumari <67224174+Shivani-Kumari-1@users.noreply.github.com>
2020-12-28 19:01:24 +0530

    Add files via upload
    0 0 0 0 0
    0 0 0 0 0
    0 0 1 0 0
    0 0 0 0 0
    0 0 0 0 0

---
+++ binary10.cpp
@@ -0,0 +1,30 @@
+/*
+
+0 0 0 0 0
+0 0 0 0 0
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 0 0
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    for(i=1;i<=5;i++)
+    {
+        if(i==3)
+                n=4;
+            else
+                n=0;
+        for(j=1;j<=5;j++)
+        {
+            cout<<" "<<n%2;
+            n=n/2;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
