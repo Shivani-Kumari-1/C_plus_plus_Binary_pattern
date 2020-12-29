9e4fe527e44b03a0360308a4526bd57ea83ed7ec
Shivani Kumari <67224174+Shivani-Kumari-1@users.noreply.github.com>
2020-12-29 17:37:30 +0530

    Add files via upload
               0
             1 1
          0 0 0
       1 1 1 1
    0 0 0 0 0

---
+++ binary22a.cpp
@@ -0,0 +1,38 @@
+/*
+
+        0
+      1 1
+    0 0 0
+  1 1 1 1
+0 0 0 0 0
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    cout<<"Enter Number = ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            cout<<" "<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            if(i==2 || i==4)
+            {
+                cout<<" "<<"1";
+            }
+            else
+            {
+                cout<<" "<<"0";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
