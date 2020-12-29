a137746d448a34d4fd62ffd976eb5b8a54262cca
Shivani Kumari <67224174+Shivani-Kumari-1@users.noreply.github.com>
2020-12-29 17:39:15 +0530

    Add files via upload
                0
             0 1
          0 1 0
       0 1 0 1
    0 1 0 1 0

---
+++ binary23a.cpp
@@ -0,0 +1,38 @@
+/*
+
+        0
+      0 1
+    0 1 0
+  0 1 0 1
+0 1 0 1 0
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    cout<<"Enter Number = ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            cout<<" "<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            if(j==2 || j==4)
+            {
+                cout<<" "<<"1";
+            }
+            else
+            {
+                cout<<" "<<"0";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
